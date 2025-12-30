5c98901c170b2cc9e044dc802973dcc71101009d
markojakob <markjakobo@gmail.com>
2025-12-30 22:32:22 +0200

    add crud tests

--- src/Backend/Tests/Todocrud.test.ts
+++ src/Backend/Tests/Todocrud.test.ts
@@ -1,15 +1,40 @@
 import { describe, it, expect } from "bun:test";
-import { saveTodo, getTodoById, removeTodo } from "../implementation";
+import { saveTodo, getTodoById, removeTodo, removeTodoByName, getTodoByName } from "../Todocrud/crud";
 
 describe("Todo CRUD Operations", () => {
     it("should create a new todo item", async () => {
-        const todo = { id: 1, title: "Test Todo", description: "aovweinm", completed: false };
+        const todo = { title: "Test Todo", description: "aovweinm", completed: false };
 
         await saveTodo(todo);
-        const fetchedTodo = await getTodoById(1);
-        expect(fetchedTodo).toEqual(todo);
+        const fetchedTodo = await getTodoByName(todo.title);
+        expect(fetchedTodo?.title).toEqual(todo.title);
 
-        removeTodo(todo.id)
+        removeTodoByName(todo.title);
 
+    });
+
+    it("should update an existing todo item", async () => {
+        const todo = { title: "Initial Title", description: "Initial Description", completed: false };
+
+        await saveTodo(todo);
+        todo.title = "Updated Title";
+        await saveTodo(todo);
+        const updatedTodo = await getTodoByName(todo.title);
+        expect(updatedTodo?.title).toBe(todo.title);
+        removeTodoByName(todo.title);
+
+    });
+
+    it("should delete a todo item", async () => {
+        const todo = { title: "To Be Deleted", description: "This will be deleted", completed: false };
+
+        await saveTodo(todo);
+        const fetchedTodo = await getTodoByName(todo.title);
+        expect(fetchedTodo?.title).toEqual(todo.title);
+        removeTodoByName(todo.title);
+        const deletedTodo = await getTodoByName(todo.title);
+        expect(deletedTodo).toBeNull();
+
+        removeTodoByName(todo.title);
     });
 });
---
+++ src/Backend/Todocrud/crud.ts
@@ -0,0 +1,21 @@
+import prisma from "../db"
+import type { Todo } from "../../../prisma/generated/client"
+
+export async function saveTodo(todo: Omit<Todo, 'id'>) {
+  await prisma.todo.create({ data: todo })
+}
+
+export async function getTodoById(id: number) {
+  return await prisma.todo.findUnique({ where: { id } })
+}
+
+export async function removeTodoByName(title: string) {
+  return await prisma.todo.deleteMany({ where: { title } })
+}
+export async function removeTodo(id: number) {
+  await prisma.todo.delete({ where: { id } })
+}
+
+export async function getTodoByName(title: string) {
+    return await prisma.todo.findFirst({ where: { title}})
+}
