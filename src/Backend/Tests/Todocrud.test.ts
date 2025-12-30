a17af3f5502e5b05f2044ecde53c4dc3e8e551e1
Kaspar Bergert <kasparbergert6@gmail.com>
2025-12-30 21:49:22 +0200

    implement saveTodo, getTodoById, removeTodo

--- src/Backend/Tests/Todocrud.test.ts
+++ src/Backend/Tests/Todocrud.test.ts
@@ -1,11 +1,15 @@
 import { describe, it, expect } from "bun:test";
+import { saveTodo, getTodoById, removeTodo } from "../implementation";
 
 describe("Todo CRUD Operations", () => {
-    it("should create a new todo item", () => {
-        const todo = { id: 1, title: "Test Todo", completed: false };
+    it("should create a new todo item", async () => {
+        const todo = { id: 1, title: "Test Todo", description: "aovweinm", completed: false };
 
-        saveTodo(todo);
-        const fetchedTodo = getTodoById(1);
+        await saveTodo(todo);
+        const fetchedTodo = await getTodoById(1);
         expect(fetchedTodo).toEqual(todo);
+
+        removeTodo(todo.id)
+
     });
 });
